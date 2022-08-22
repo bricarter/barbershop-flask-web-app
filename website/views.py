7e354855aafa0c9d14053337f5a9b0e2b1080ae2
bricarter <101752437+bricarter@users.noreply.github.com>
2022-08-22 12:14:12 -0500

    defined book_appt view/route

--- website/views.py
+++ website/views.py
@@ -5,3 +5,7 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html")
+
+@views.route('/book_appt')
+def book_appt():
+    return render_template("book_appt.html")    
