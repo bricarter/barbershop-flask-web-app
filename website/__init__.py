12199f17b61c71dc261c553e30021393f24ac79f
bricarter <101752437+bricarter@users.noreply.github.com>
2022-08-23 12:14:10 -0500

    initialized + created database

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "barbershop.db"
+
 
 def create_app():
     app = Flask(__name__)
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
    
-    from .views import views
+    from .views import views 
 
     app.register_blueprint(views, url_prefix='/')
+    
+    from .models import Appointment
 
+    create_database(app)
 
     return app
 
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Database created!')
