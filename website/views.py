ba556bdff830c98c9e1ed2e9c4803fa9cff3f320
bricarter <101752437+bricarter@users.noreply.github.com>
2022-08-23 12:16:33 -0500

    create new_appt functionality

--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from . import db
+from .models import Appointment
 
 views = Blueprint('views', __name__)
 
@@ -6,6 +8,24 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("home.html")
 
-@views.route('/book_appt')
+@views.route('/book_appt', methods = ['GET', 'POST'])
 def book_appt():
+    if request.method == 'POST':
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        appt_date = request.form.get('appt_date')
+        appt_time = request.form.get('appt_time')
+
+        conflicting_appt = Appointment.query.filter_by(appt_date = appt_date).filter_by(appt_time = appt_time).first()
+
+        if conflicting_appt:
+            flash('This slot is not available please choose a different date or time.', category='error')
+        else:
+            new_appt = Appointment(first_name=first_name, last_name=last_name, appt_date=appt_date, appt_time=appt_time)
+            db.session.add(new_appt)
+            db.session.commit()
+
+            flash('Appointment booked successfully!', category='success')
+            return redirect(url_for('views.home'))
+
     return render_template("book_appt.html")    
