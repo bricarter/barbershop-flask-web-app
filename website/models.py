821439fd7448e8b431d4ae765df63b553472d656
bricarter <101752437+bricarter@users.noreply.github.com>
2022-08-23 12:11:21 -0500

    defined models schema

--- website/models.py
+++ website/models.py
@@ -1,1 +1,10 @@
-pass
+from . import db
+from sqlalchemy.sql import func
+
+class Appointment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(150))
+    last_name = db.Column(db.String(150))
+    appt_date = db.Column(db.String(20))
+    appt_time = db.Column(db.String(20))
+    set_date = db.Column(db.DateTime(timezone=True), default=func.now())
